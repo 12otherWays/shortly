c73d50ed4a4fc20685f1ba18d8d23dfe8b65c2d8
12OtherWays <reeturajsinghaniya31@gmail.com>
2023-10-26 14:40:14 +0530

    added hero section

--- app/components/Homepage.js
+++ app/components/Homepage.js
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
 import Questions from "./homepage/Questions";
+import HeroSection from "./homepage/HeroSection";
 
 function Homepage() {
   return (
     <Box>
+      <HeroSection />
       <Questions />
     </Box>
   );
